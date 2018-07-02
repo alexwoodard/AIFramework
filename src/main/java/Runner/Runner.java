5be3b858c929587d15edf3b3ab7ef7c874c98c82
Alex Woodard <awoodard@godaddy.com>
2018-07-02 15:13:35 -0700

    complete refactor

--- src/main/java/Runner/Runner.java
+++ src/main/java/Runner/Runner.java
@@ -7,38 +7,38 @@ import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import clickablefinder.BfsAlgorithm;
-import clickablefinder.PageComponent;
-import clickablefinder.XpathSelectorBuilder;
+import clickablefinder.ClickerCrawler2;
 
 public class Runner {
 
 	public static void main(String args[]) throws InterruptedException, IOException {
 
-		XpathSelectorBuilder xpathSelectorBuilder = new XpathSelectorBuilder();
 		String gridAddress = "http://10.32.64.63:4444/wd/hub";
-		DesiredCapabilities capability = DesiredCapabilities.chrome();
-		RemoteWebDriver rwd = new RemoteWebDriver(new URL(gridAddress), capability);
-		BfsAlgorithm bfs_algorithm = new BfsAlgorithm();
-		rwd.get("https://www.godaddy.com");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		ChromeDriver rwd =new ChromeDriver();
+//		RemoteWebDriver rwd = new RemoteWebDriver(new URL(gridAddress), capability);
+		ClickerCrawler2 crawler = new ClickerCrawler2();
+		rwd.get("https://www.godaddy.com/websites/website-builder");
 		rwd.manage().window().maximize();
+		Dimension d = new Dimension(2024, 900);
+		rwd.manage().window().setSize(d);
 		synchronized (rwd) {
 			rwd.wait(5000);
 			File scrFile = ((TakesScreenshot)rwd).getScreenshotAs(OutputType.FILE);
-			// Now you can do whatever you need to do with it, for example copy somewhere
 			FileUtils.copyFile(scrFile, new File("screenshot.png"));
 		}
 
-		WebElement rootElement = rwd.findElement(By.cssSelector("body"));
-		String rootXpathSelector = xpathSelectorBuilder.generateXPATH(rootElement, "");
-		PageComponent pc = new PageComponent(rootXpathSelector, null, null);
-		String result = bfs_algorithm.search(pc, rwd);
+		crawler.search(rwd);
 		System.out.println("Crawl Complete");
 	}
 }
---
+++ src/main/java/component/Clickable.java
@@ -0,0 +1,15 @@
+package component;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+
+public class Clickable {
+
+	private Dimension dimension;
+	private Point point;
+	private String text;
+	private int size;
+	private int x;
+	private int y;
+	private String href;
+}
